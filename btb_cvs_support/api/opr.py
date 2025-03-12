bb2d49592710415a32d877b60b539117199fa2ec
Ramu K <ramuk@Ramus-MacBook-Pro.local>
2025-03-13 00:35:25 +0530

    grouping moved to sales order item level

--- btb_cvs_support/api/opr.py
+++ btb_cvs_support/api/opr.py
@@ -6,49 +6,49 @@ def get_pending_quantities(sales_order_number: str, opr_name: str):
     ordered_items = get_ordered_items(sales_order_number, opr_name)
     output = []
     for item in items:
-        code = item["item_code"]
+        detail = item["so_detail"]
         ordered_qty = 0
-        if(code in ordered_items):
-            ordered_qty = ordered_items[code] 
+        if(detail in ordered_items):
+            ordered_qty = ordered_items[detail] 
         if(item["qty"] == ordered_qty):
             continue
-        output.append({"item_code": code, "description": item["description"], "uom": item["uom"],
+        output.append({"so_detail": detail, "description": item["description"], "item_code": item["item_code"], "uom": item["uom"],
                        "rate": item["rate"], "quantity": item["qty"] - ordered_qty,
                        "custom_manufactured_item": item["custom_manufactured_item"]})
     return output
 
 def get_items(sales_order_number: str):
     sql = f"""
-        select i.item_code, i.description, soi.rate, soi.uom, sum(qty) qty,
+        select soi.name so_detail, soi.description, soi.item_code, soi.rate, soi.uom, qty,
             i.custom_manufactured_item
         from `tabSales Order Item` soi
-        join tabItem i on soi.item_code = i.item_code
+        join `tabItem` i on soi.item_code = i.item_code
         where soi.parent = '{sales_order_number}'
-        group by i.item_code, i.description, soi.rate, soi.uom
+        
     """
     return frappe.db.sql(sql, as_dict=1)
 
 def get_ordered_items(sales_order_number: str, opr_name: str):
     sql = f"""
-        Select qt.item_code, sum(qt.quantity) qty
+        Select qt.so_detail, sum(qt.quantity) qty
         from `tabQuantities Table` qt
         join `tabOrder Processing Request` opr on qt.parent = opr.name
         where opr.sales_order = '{sales_order_number}'
         and opr.name != '{opr_name}'
-        group by qt.item_code
+        group by qt.so_detail
         
         union all
         
-        select acc.item, sum(acc.quantity)
+        select acc.so_detail, sum(acc.quantity)
         from `tabAccessories` acc
         join `tabOrder Processing Request` opr on acc.parent = opr.name
         where opr.sales_order = '{sales_order_number}'
         and opr.name != '{opr_name}'
-        group by acc.item
+        group by acc.so_detail
     """
     items = frappe.db.sql(sql, as_dict=1)
     output = {}
     for item in items:
         print(item)
-        output[item["item_code"]] = item["qty"]
+        output[item["so_detail"]] = item["qty"]
     return output
