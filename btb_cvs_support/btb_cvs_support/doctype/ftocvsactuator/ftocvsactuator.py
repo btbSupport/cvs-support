4aed2c4c23049fd7be66edcbf46b920bf7e34c2f
Ramu Kulandaivelu <ramu_erode@biztbaa.com>
2025-04-11 19:32:48 +0000

    Feature Type Object is added

---
+++ btb_cvs_support/btb_cvs_support/doctype/ftocvsactuator/ftocvsactuator.py
@@ -0,0 +1,6 @@
+
+from frappe.model.document import Document
+
+
+class FTOCVSActuator(Document):
+	pass
---
+++ btb_cvs_support/btb_cvs_support/doctype/ftoflangeanglerange/ftoflangeanglerange.py
@@ -0,0 +1,6 @@
+
+from frappe.model.document import Document
+
+
+class FTOFlangeAngleRange(Document):
+	pass
