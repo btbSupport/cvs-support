0158f8aa7adcf4ba75d75f1a131fc51dcd177d02
ramu-erode <frappe@hr-erp.com>
2025-08-04 10:47:41 +0000

    Feature type tables changes

---
+++ btb_cvs_support/btb_cvs_support/doctype/ftoactuator_area/ftoactuator_area.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, BizTBaa and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("FTOActuator_Area", {
+// 	refresh(frm) {
+
+// 	},
+// });
