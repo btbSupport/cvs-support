74169e2e0b701d4d50afe6063b32e60fcadd438d
Komala <komala@biztbaa.com>
2025-09-22 10:37:23 +0000

    get site

--- btb_cvs_support/api/site_info.py
+++ btb_cvs_support/api/site_info.py
@@ -3,7 +3,7 @@ import json
 from frappe.utils import get_site_name
 
 def get_site_config():
-    frappe.log(f"Error: frappe.local.request.header in {frappe.local.request.header}")
+    frappe.log(f"Error: frappe.local.request.header in {frappe.local.site}")
     site_name = get_site_name(frappe.local.request.header)
     print(f"site_name {site_name}")
     site_config_path = "../sites/"+site_name+"/site_config.json"
