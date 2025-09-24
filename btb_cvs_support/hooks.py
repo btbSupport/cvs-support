1a55455779a8f4a2507dc7e7a10513f030a5775c
Komala <komala@biztbaa.com>
2025-09-24 04:26:01 +0000

    hooks change

--- btb_cvs_support/hooks.py
+++ btb_cvs_support/hooks.py
@@ -283,19 +283,12 @@ fixtures = [
     {
         "doctype": "Client Script",
         "filters": [
-            ["name", "in", [
-                "opr_controller", "Order Processing Request-Client", "Delivery Note-Client", "Sales Order-Client","Quotation_controller"
-            ]]
+            ["module","in",["Btb Cvs Support"]]
         ]
     },
     {
         "doctype": "Custom Field",
         "filters": [
-            ["fieldname", "in", [
-                "custom_manufactured_item", "custom_opr", "custom_sales_order", "custom_total_sqm", "custom_total_pcs","custom_total_discount",
-"custom_total_discount_amount", "custom_list_amount", "custom_discount", "custom_onecpq_total", "custom_cart_discount","custom_onecpq_items",
-"custom_apply_discount", "custom_customizable","letter","letter_details","quotation_terms","quotation_term_details","tag_ref","notes"
-            ]],
             ["module","in",["Btb Cvs Support"]]
         ]
     }
