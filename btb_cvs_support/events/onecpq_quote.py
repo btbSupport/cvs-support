2602fc053af4a6771f3b12df9a35560f76f25d35
Komala <komala@biztbaa.com>
2025-10-30 05:58:01 +0000

    limiti from settings

--- btb_cvs_support/events/onecpq_quote.py
+++ btb_cvs_support/events/onecpq_quote.py
@@ -37,7 +37,11 @@ def get_items(quote_name: str):
 def before_save_cart(doc, method = None):
     print('get_config allow_cpq before_save_cart',get_config('allow_cpq') )
     if(get_config('allow_cpq')==None or get_config('allow_cpq') == 0): return
-    # if(doc.discount != None and (doc.discount <-100 or doc.discount > 30)):frappe.throw('Discount % not in the approved limit.')
+
+    settings = frappe.get_doc("cvsSettings")
+    min_discount_limit = settings.get("cart_minimum_discount")
+    max_discount_limit = settings.get("cart_maximum_discount")
+    if(doc.discount != None and (doc.discount < min_discount_limit or doc.discount > max_discount_limit)):frappe.throw('Discount % not in the approved limit.')
     if(doc.unit_price == 0): doc.valid = 0
 
 @frappe.whitelist()
